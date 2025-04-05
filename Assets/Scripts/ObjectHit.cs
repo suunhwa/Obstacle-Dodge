b250211d41332c9ea11593ed270e66132cb508bc
suunhwa <snna628@gmail.com>
2025-04-05 13:18:50 +0900

    feat: Implement player movement, map setup, and color change on collision

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        Debug.Log("Something hit me!");
+    }
+}
